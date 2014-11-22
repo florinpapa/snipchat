9de1e62844c6f1f379a397c08c533e03a1dcdd0b
Andrei Oprea <andrei.br92@gmail.com>
2014-11-22 15:41:27 +0200

    Add new page

--- snipchatapp/urls.py
+++ snipchatapp/urls.py
@@ -3,4 +3,5 @@ from snipchatapp import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'add_snippet/', views.add_snippet, name='add_snippet'),
 )
--- snipchatapp/views.py
+++ snipchatapp/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 from snipchatapp.models import Snippets, Users, Comments
 
 def index(request):
-  context = {'snippet': Snippets.objects.order_by('-pub_date')[:1][0]}
-  return render(request, 'snippet/index.html', context)
+    context = {'snippet': Snippets.objects.order_by('-pub_date')[:1][0]}
+    return render(request, 'snippet/index.html', context)
+
+def add_snippet(request):
+    return render(request, 'snippet/add_snippet.html')
