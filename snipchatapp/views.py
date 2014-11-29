cb0775d07bcc4572650fd4b2610ecd070a5c32fe
Andrei Oprea <andrei.br92@gmail.com>
2014-11-30 00:08:31 +0200

    Add versioning

--- snipchatapp/views.py
+++ snipchatapp/views.py
@@ -25,7 +25,11 @@ def index(request):
     return render(request, 'snippet/index.html', context)
 
 def view_snippet(request, snippet_id):
-    context = {'snippet': Snippets.objects.get(identifier=snippet_id)}
+    snippet = Snippets.objects.get(identifier=snippet_id)
+    context = {
+            'snippet': snippet,
+            'versions': snippet.history.split('|')[:-1] 
+            }
     return render(request, 'snippet/index.html', context)
 
 def add_snippet(request):
@@ -35,6 +39,7 @@ def add_snippet(request):
         identifier = random_identifier()
         snippet = Snippets(code=code, user=user, identifier=identifier,
                            pub_date=timezone.now())
+        snippet.history = identifier + "|"
         snippet.save()
         return view_snippet(request, identifier)
     else:
@@ -42,7 +47,6 @@ def add_snippet(request):
 
 def new_version(request, snippet_id):
     if request.method == 'POST':
-        print "here"
         old_snippet = Snippets.objects.get(identifier=snippet_id)
         code = request.POST['code']
         user = Users.objects.all()[0]
@@ -50,6 +54,7 @@ def new_version(request, snippet_id):
         snippet = Snippets(code=code, user=user, identifier=identifier,
                            revision=old_snippet.revision + 1,
                            pub_date=timezone.now())
+        snippet.history = old_snippet.history + identifier + "|"
         snippet.save()
         response = {
             'success': 'true',
