4a18b57844882a41e96be9b83d0451a0e7d4015c
florinpapa <florin.papa192@gmail.com>
2014-11-30 15:11:06 +0200

    Fix redirect bug

--- snipchatapp/static/javascript/main.js
+++ snipchatapp/static/javascript/main.js
@@ -48,7 +48,7 @@
       code: code,
     }).success(function(data) {
       console.log(data);
-      window.location.href = "http://localhost:8000/snippet/snippet/" + data.identifier;
+      window.location.href = "http://localhost:8000/snippet/" + data.identifier;
     }).fail(function(data) {
       console.log(data);
     });
