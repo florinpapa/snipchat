871c867bb84852dc4717d1d4ea8642901a64da1a
Andrei Oprea <andrei.br92@gmail.com>
2014-11-30 18:14:01 +0200

    Add login logic

--- snipchatapp/views.py
+++ snipchatapp/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import redirect
 from snipchatapp.models import Snippets, Comments
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as django_login
 from random import randrange
+from django import forms
 
 def generate():
   if randrange(10) > 5:
@@ -46,8 +48,31 @@ def register(request):
         'form': form,
     })
 
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username',
+                               max_length=100, required=True)
+    password = forms.CharField(label='Password',
+                               max_length=100, required=True,
+                               widget=forms.PasswordInput())
+    class Meta:
+        model = User
+
 def login(request):
-    pass
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            user = authenticate(username=username,
+                                     password=password)
+            if user is not None and user.is_active:
+                django_login(request, user)
+                return redirect('profile')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {
+        'form': form    
+    })
 
 def log_out(request):
     logout(request)
